b31a96b02a3e329916bd5a86c3bf2af7c3c01d6d
Sampreeti Fondekar <sampreeti1010@gmail.com>
2026-06-24 12:07:05 +0530

    Initial commit

---
+++ src/P1_1.kt
@@ -0,0 +1,38 @@
+fun main(){
+//    var j: Int
+//    j=20
+//    println("Integer Value= "+j)
+//    val k: Float
+//    k= 3.6F
+//    println("Integer Value= "+k)
+//    var i: Char
+//    i= 'A'
+//    println("Character Value= "+i)
+//    var l: String
+//    l = "SDF"
+//    println("String Value= "+l)
+//    var m: Boolean
+//    m = true
+//    println("Boolean Value= "+m)
+//    var n: Double
+//    n = 3.14
+//    println("Double Value= "+n)
+//    var a: Long
+//    a = 165948237
+//    println("Long Value= "+a)
+//    var b: Short
+//    b = 2
+//    println("Short Value= "+b)
+//    var c: Byte
+//    c = 122
+//    println("Byte Value= "+c)
+    var x=10
+    var y=5
+    println("x=$x,y=$y")
+    println("x+y=${x+y}")
+    println(y)
+    println("y="+y)
+    val pi = 3.14159265
+    println("pi=")
+    print(String.format("%.2f", pi))
+}
---
+++ src/P1_2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val i=10000
+    val j : Double=i.toDouble()
+    println("Integer value= $i")
+    println("Double value(From Integer)= $j")
+    val k:String="25"
+    println("String value= $k")
+    val l:Int=k.toInt()
+    println("Int value(From String)= $l")
+    val m:String="25.33"
+    println("String value= $m")
+    val a:Double=m.toDouble()
+    println("Double value(From String)= $a")
+}
